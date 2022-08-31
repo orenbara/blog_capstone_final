98010017f104ad5c56852f16e2bd038e18a52a14
orenbara <orenbaranovsky@gmail.com>
2022-08-31 17:04:29 +0300

    added postgre

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("secret_key")
 # Test that main gets your environment variable  - secret_key
-print(os.environ.get("secret_key"))
+# print(os.environ.get("secret_key"))
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -30,7 +30,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
